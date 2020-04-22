a3f661b2bf52d7b4868aca5e584640684b82083c
zhanglingpeng <742285653@qq.com>
2020-04-22 22:43:15 +0800

    12

--- springCloudDemo/service_feign/src/main/java/com/lianxi/service_feign/controller/DemoController.java
+++ springCloudDemo/service_feign/src/main/java/com/lianxi/service_feign/controller/DemoController.java
@@ -1,20 +1,22 @@
 package com.lianxi.service_feign.controller;
 
+import com.lianxi.service_feign.entity.User;
 import com.lianxi.service_feign.feigninterface.HiInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class DemoController {
 
     @Autowired
-
     HiInterface hiInterface;
 
     @RequestMapping("/hi")
-    public String hi (){
+    public List<User> hi (){
 
         return hiInterface.sayHi();
 
--- springCloudDemo/service_feign/src/main/java/com/lianxi/service_feign/feigninterface/HiInterface.java
+++ springCloudDemo/service_feign/src/main/java/com/lianxi/service_feign/feigninterface/HiInterface.java
@@ -1,13 +1,15 @@
 package com.lianxi.service_feign.feigninterface;
 
 
+import com.lianxi.service_feign.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value ="service-hi")
-public interface HiInterface {
-    @RequestMapping(value = "/hi" , method = RequestMethod.GET)
+import java.util.List;
 
-    String sayHi();
+@FeignClient(value ="service-hi")
+public interface HiInterface  {
+    @RequestMapping(value = "/api/user/findAll" , method = RequestMethod.GET)
+    List<User> sayHi();
 }
